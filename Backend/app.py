74897bea85e787b3384c60ca5fe0349c98ed77f5
Nathan La <nala@uci.edu>
2023-11-04 17:40:04 -0700

    redid the get results functions for the back end

--- Backend/app.py
+++ Backend/app.py
@@ -11,14 +11,21 @@ OUTSIDE_UCI_RADIUS = 1000
 app = Flask(__name__)
 CORS(app)
 
-UTC = {}
-Plaza = {}
-OutSideUCI = {}
-InUCI = {}
+UTC = []
+Plaza = []
+OutSideUCI = []
+InUCI = []
+
+@app.route('/')
+def getResults():
+    
+    getResultsUTC()
+    getResultsInUCI()
+    getResultsPlaza()
+    getResultsOutUCI()
 
 
 
-@app.route('/UTC')
 def getResultsUTC():
         
     url = "https://api.yelp.com/v3/businesses/search?latitude=33.650551206116795&longitude=-117.83889468961557&term=food&radius=360&sort_by=best_match&limit=50"
@@ -28,12 +35,10 @@ def getResultsUTC():
         "Authorization": "Bearer bB0ZapOxzLjsHScdSZc2iOD_8PjTuGXiLvEeB2UseuIZKBV4KRPGKZ6TgLchYZfzXxX219srxe_GLnq8eaOPpx5H1M9TmCODk7Cb6JuTZyWASGYrSIPz24oXBp1GZXYx"
     }
 
-    UTC = requests.get(url, headers=headers).json()
-
-    return jsonify(UTC)
+    UTC = _parse(requests.get(url, headers=headers).json())
 
 
-@app.route('/CampusPlaza')
+
 def getResultsPlaza():
         
     url = "https://api.yelp.com/v3/businesses/search?latitude=33.64960859868817&longitude=-117.83154993786337&term=food&radius=220&sort_by=best_match&limit=20"
@@ -43,13 +48,11 @@ def getResultsPlaza():
         "Authorization": "Bearer bB0ZapOxzLjsHScdSZc2iOD_8PjTuGXiLvEeB2UseuIZKBV4KRPGKZ6TgLchYZfzXxX219srxe_GLnq8eaOPpx5H1M9TmCODk7Cb6JuTZyWASGYrSIPz24oXBp1GZXYx"
     }
 
-    Plaza = requests.get(url, headers=headers).json()
-
-    return jsonify(Plaza)
+    Plaza = _parse(requests.get(url, headers=headers).json())
 
 
-@app.route('/OutsideUCI')
-def getResultsOutUCI():
+
+def getResultsOutUCI() -> 'json':
 
     url = "https://api.yelp.com/v3/businesses/search?latitude=33.64602211664342&longitude=-117.84272864126669&term=food&radius=10000&sort_by=best_match&limit=20"
 
@@ -58,24 +61,27 @@ def getResultsOutUCI():
         "Authorization": "Bearer bB0ZapOxzLjsHScdSZc2iOD_8PjTuGXiLvEeB2UseuIZKBV4KRPGKZ6TgLchYZfzXxX219srxe_GLnq8eaOPpx5H1M9TmCODk7Cb6JuTZyWASGYrSIPz24oXBp1GZXYx"
     }
     
-    OutSideUCI = requests.get(url, headers=headers).json()
+    OutSideUCI = _parse(requests.get(url, headers=headers).json())
         
-    for resturant in UTC['businesses']: 
-        deleteResturant(OutSideUCI['businesses'], resturant['name'])
+    for resturant in UTC: 
+        deleteResturant(OutSideUCI, resturant)
     
-    for resturant in Plaza['businesses']:
-        del OutSideUCI['businesses'][resturant['name']]
+    for resturant in Plaza:
+        deleteResturant(OutSideUCI, resturant)
 
 
-    return jsonify(OutSideUCI.json())
 
 
-def deleteResturant(del_list, string):
+def deleteResturant(del_list : list, resturant_rm : dict) -> None:
+    index = 0
     for resturant in del_list:
+        if resturant_rm == resturant:
+            del del_list[index]
+            break
+        index += 1
+            
         
 
-
-@app.route('/InsideUCI')
 def getResultsInUCI():
         
     with open('./on_campus_locations.json', 'r') as file:
@@ -84,22 +90,10 @@ def getResultsInUCI():
     return jsonify(InUCI.json())
 
 
-
-
-
-
-
-    response = requests.get(url, headers=headers)
-    locations = _parse(response.json())
-
-    return jsonify(locations)
-
 def _parse(res: dict) -> list:
     '''Returns a list that only has the needed attributes of the locations'''
     copy_of_res = res.copy()
 
-    condensed = dict()
-    locations = []
     needed_attributes = ['name', 'image_url', 'rating', 'price', 'location', 'display_phone', 'distance']
     for location in copy_of_res['businesses']:
         attributes_to_delete = []
